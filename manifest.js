9c62cf155d88ea2183524c14385907b8e696e95f
prasanna80564 <20039859@mydbs.ie>
2025-06-30 19:12:03 +0100

    update in manifest.js

---
+++ manifest.js
@@ -0,0 +1,16 @@
+{
+  "manifest_version": 3,
+  "name": "WebSecGuard",
+  "version": "1.0",
+  "description": "Real-time XSS and CSRF vulnerability detection",
+  "permissions": [
+    "webRequest",
+    "storage",
+    "activeTab",
+    "scripting",
+    "tabs",
+    "notifications",
+    "cookies",
+    "alarms"
+  ]
+}
