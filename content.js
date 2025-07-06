2ad5dd773ad854bc5fc4031ad9edb3772000b86a
prasanna80564 <20039859@mydbs.ie>
2025-07-06 19:41:42 +0100

    content.js

---
+++ content.js
@@ -0,0 +1,11 @@
+let isMonitoring = true;
+const debugMode = false;
+const falsePositiveUrls = [
+  'chrome://', 
+  'about:',
+  'edge://',
+  'opera://',
+  'moz-extension://',
+  'chrome-extension://',
+  'file://'
+];
